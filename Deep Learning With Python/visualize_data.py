1a47191912e53467cb054fa9deee4647770b51d5
rehmanm <muhammad.abdulrehman@gmail.com>
2018-06-18 18:45:05 +0500

    Adding Visualization and Data Augmentation

---
+++ Deep Learning With Python/visualize_data.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+
+def visualize_data(history):
+
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+
+    epochs = range(1, len(acc) + 1)
+
+    plt.plot(epochs, acc, 'bo', label = 'Training Accuraccy')
+    plt.plot(epochs, val_acc, 'b', label = 'Validation Accuracy')
+    plt.title("Training and Validation Accuracy")
+    plt.legend()
+
+    plt.figure()
+
+    plt.plot(epochs, loss, 'bo', label = 'Training Loss')
+    plt.plot(epochs, val_loss, 'b', label = 'Validation Loss')
+    plt.title("Training and Validation Loss")
+    plt.legend()
+    
+    plt.show()
